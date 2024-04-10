05386eeb162a71803d1f6252730ef8575f01b71e
Luka Jengic <luka.jengic@gmail.com>
2024-04-10 11:38:32 -0400

    add simple ble scanner

--- package/app.py
+++ package/app.py
@@ -1,12 +1,15 @@
 import sys
 import time
 from PyQt5 import QtWidgets
-
+from package.ble_api.scan import BLE_Scanner
 
 class MainWindow(QtWidgets.QMainWindow):
+    
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
-        
+        self.ble_scanner = BLE_Scanner()
+        self.ble_scanner.scan_devices()
+
 #run application
 
 def run():
