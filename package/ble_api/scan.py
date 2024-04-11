0175dac1fb624c4b21bdf5783f86ebdabd58ad86
Luka Jengic <luka.jengic@gmail.com>
2024-04-11 11:01:42 -0400

    send signal with discovered devices when BLE scanning

--- package/ble_api/scan.py
+++ package/ble_api/scan.py
@@ -2,24 +2,25 @@ from PyQt5.QtBluetooth import QBluetoothDeviceDiscoveryAgent
 from PyQt5.QtCore import QObject, pyqtSignal, pyqtSlot
 
 class BLE_Scanner(QObject):  
-    #scannerOutputMessage = pyqtSignal(str)
+    devices_scanned_message = pyqtSignal(list)
+    discoverd_devices_list = None
 
     def __init__(self):
         QObject.__init__(self)
         self.discoveryAgent = QBluetoothDeviceDiscoveryAgent()
         self.discoveryAgent.setLowEnergyDiscoveryTimeout(5000)
         self.discoveryAgent.finished.connect(self.get_devices)
-        self.discoverd_devices_list = None
 
     def scan_devices(self):
         print("Scanning\n")
-        #self.scannerOutputMessage.emit(">>Scanning\n")
         self.discoveryAgent.start()
 
     def get_devices(self):
         self.discoverd_devices_list = self.discoveryAgent.discoveredDevices()
         self.print_devices()
+        self.devices_scanned_message.emit(self.discoverd_devices_list)
 
     def print_devices(self):
         for obj in self.discoverd_devices_list:
             print(obj.name())
+
