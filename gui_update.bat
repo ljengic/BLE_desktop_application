15ad46372910b65cf82c37bb9ca03fe80b30b316
Luka Jengic <luka.jengic@gmail.com>
2024-04-24 13:56:45 -0400

    update gui compile script

--- gui_update.bat
+++ gui_update.bat
@@ -4,3 +4,6 @@ python -m PyQt5.uic.pyuic -x ./package/ble/gui_ble.ui -o ./package/ble/gui_ble.p
 python -m PyQt5.uic.pyuic -x ./package/patients/gui_patients.ui -o ./package/patients/gui_patients.py
 python -m PyQt5.uic.pyuic -x ./package/measure/gui_measure.ui -o ./package/measure/gui_measure.py
 python -m PyQt5.uic.pyuic -x ./package/results/gui_results.ui -o ./package/results/gui_results.py
+
+python -m PyQt5.pyrcc_main -o ./images/resources_rc.py ./images/resources.qrc
+::copy "./images/resources_rc.py" "./resources_rc.py"
