3e6f4f01266e19d7ab3b5f6b0c3768aff3931bf2
Luka Jengic <luka.jengic@gmail.com>
2024-04-30 15:40:50 -0400

    update gui generate script

--- gui_update.bat
+++ gui_update.bat
@@ -2,6 +2,8 @@ python -m PyQt5.uic.pyuic -x ./package/main_app/gui.ui -o ./package/main_app/gui
 python -m PyQt5.uic.pyuic -x ./package/home/gui_home.ui -o ./package/home/gui_home.py
 python -m PyQt5.uic.pyuic -x ./package/ble/gui_ble.ui -o ./package/ble/gui_ble.py
 python -m PyQt5.uic.pyuic -x ./package/measure/gui_measure.ui -o ./package/measure/gui_measure.py
+python -m PyQt5.uic.pyuic -x ./package/measure/gui_add_medication.ui -o ./package/measure/gui_add_medication.py
+python -m PyQt5.uic.pyuic -x ./package/measure/gui_medicine.ui -o ./package/measure/gui_medicine.py
 python -m PyQt5.uic.pyuic -x ./package/results/gui_results.ui -o ./package/results/gui_results.py
 
 python -m PyQt5.pyrcc_main -o ./images/resources_rc.py ./images/resources.qrc
