a97365f789fad76afe97967013f7d839d04b7834
Luka Jengic <luka.jengic@gmail.com>
2024-05-16 17:05:21 -0400

    update paths

--- package/tools/paths.py
+++ package/tools/paths.py
@@ -7,10 +7,21 @@ class Paths:
 
         self.patient_file_path = self.make_patient_path()
         self.temp_data_file = self.make_temp_path()
+        self.bioz_data_file_frek = self.make_bioz_path_frek()
+        self.bioz_data_file_5 = self.make_bioz_path_5()
+        self.bioz_data_file_50 = self.make_bioz_path_50()
+        self.bioz_data_file_100 = self.make_bioz_path_100()
+        self.bioz_data_file_200 = self.make_bioz_path_200()
         self.raw_data_file = self.make_raw_path()
+
 
         self.make_patient_file()
         self.make_temp_file()
+        self.make_bioz_file_frek()
+        self.make_bioz_file_5()
+        self.make_bioz_file_50()
+        self.make_bioz_file_100()
+        self.make_bioz_file_200()
         self.make_raw_file()
 
 ######################################################################
@@ -21,6 +32,21 @@ class Paths:
 
     def make_temp_path(self):
         return self.folder_path + '/temp_data.csv'
+
+    def make_bioz_path_frek(self):
+        return self.folder_path + '/bioz_data_frek.csv'
+
+    def make_bioz_path_5(self):
+        return self.folder_path + '/bioz_data_5.csv'
+
+    def make_bioz_path_50(self):
+        return self.folder_path + '/bioz_data_50.csv'
+
+    def make_bioz_path_100(self):
+        return self.folder_path + '/bioz_data_100.csv'
+
+    def make_bioz_path_200(self):
+        return self.folder_path + '/bioz_data_200.csv'
 
     def make_raw_path(self):
         return self.folder_path + '/raw_data.csv'
@@ -33,6 +59,21 @@ class Paths:
 
     def make_temp_file(self):
         open( self.temp_data_file, 'a', newline='')
+
+    def make_bioz_file_frek(self):
+        open( self.bioz_data_file_frek, 'a', newline='')
+
+    def make_bioz_file_5(self):
+        open( self.bioz_data_file_5, 'a', newline='')
+
+    def make_bioz_file_50(self):
+        open( self.bioz_data_file_50, 'a', newline='')
+
+    def make_bioz_file_100(self):
+        open( self.bioz_data_file_100, 'a', newline='')
+
+    def make_bioz_file_200(self):
+        open( self.bioz_data_file_200, 'a', newline='')
 
     def make_raw_file(self):
         open(self.raw_data_file, 'a', newline='')
